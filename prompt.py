54dfc27ca84fd6422e3be313ecaedfe4f473ba8d
Benjamin Quinn <benjamin.quinn@gmail.com>
2025-04-26 13:29:23 -0400

    Use langchain

--- prompt.py
+++ prompt.py
@@ -1,29 +1,22 @@
 import csv
-import json
-import time
-import re
 import logging
+import time
+from typing import List, Optional, TypedDict
 
-import tiktoken
+from langchain_ollama import ChatOllama
+from pydantic import BaseModel, Field
 
-import ollama
-from ollama import chat
-from ollama import ChatResponse
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.embeddings import OllamaEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
 
-import chromadb
+from langgraph.graph import START, StateGraph
 
 
 logging.basicConfig(level=logging.INFO)
-
 logging.getLogger("httpx").setLevel(logging.WARN)
-
 log = logging.getLogger(__name__)
-
-
-def num_tokens_from_string(string: str, encoding_name: str) -> int:
-    encoding = tiktoken.get_encoding(encoding_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
 
 
 embedding_model = "nomic-embed-text"
@@ -35,283 +28,311 @@ accounts_file = "./data/accounts.csv"
 categories_file = "./data/categories.csv"
 output_file = "./out/transactions-out.csv"
 
-
-def read_accounts():
-    with open(accounts_file, "r") as f:
-        r = csv.DictReader(f)
-        return [
-            {
-                "Account": row["Account"],
-                "Account #": row["Account #"],
-                "Class": row["Class"],
-                "Group": row["Group"],
-            }
-            for row in r
-        ]
+# Minimum reported confidence to accept a categorization
+confidence_cuttoff = 80
+# High level of confidence, which will skip retries if reported
+high_confidence_level = 97
+# Maximum number of retries to categorize
+max_attempts = 5
 
 
-def read_categories():
+llm = ChatOllama(model=model, keep_alive='10m')
+
+
+def read_categories() -> List[str]:
     with open(categories_file, "r") as cats_f:
         r = csv.DictReader(cats_f)
         return [row["Category"] for row in r]
 
 
-def read_transactions():
-    with open(transaction_file, "r") as trans_f:
+cats = read_categories()
 
-        r = csv.DictReader(trans_f)
 
-        return [{k: v for k, v in row.items() if k and v} for row in r]
+class Transaction(TypedDict):
+    date: str
+    description: str
+    category: str
+    amount: str
+    account: str
+    account_number: str
+    institution: str
+    month: str
+    week: str
+    transaction_id: str
+    account_id: str
+    check_number: str
+    full_description: str
+    date_added: str
+
+
+class Account(TypedDict):
+    account: str
+    account_number: str
+    account_class: str
+    group: str
+
+
+class Categorization(BaseModel):
+    transaction_id: str = Field(
+        description="The transaction id from the supplied transaction"
+    )
+    account_id: str = Field(description="The account id from the supplied transaction")
+    description: str = Field(
+        description="The description from the supplied transaction"
+    )
+    category: str = Field(
+        description="The category of the transaction, from the supplied list of categories",
+        enum=cats,
+    )
+    confidence_level: int = Field(
+        description="The confidence level (in percent between 0 and 100) in your determination of the category",
+        gt=0,
+        le=100,
+    )
+    explanation: str = Field(
+        description="""A brief explanation as to how you made the categorization determination.
+ Also explain how you arrived at your chosen confidence level."""
+    )
+
+
+class State(TypedDict):
+    transaction: Transaction
+    previous_transactions: List[Document]
+    categorization: Categorization
+
+
+def read_accounts() -> List[Account]:
+    with open(accounts_file, "r") as f:
+        r = csv.DictReader(f)
+        return [
+            {
+                "account": row["Account"],
+                "account_number": row["Account #"],
+                "account_class": row["Class"],
+                "group": row["Group"],
+            }
+            for row in r
+            if row["Account"]
+        ]
+
+
+def read_transactions() -> List[Transaction]:
+    with open(transaction_file, "r") as f:
+
+        r = csv.DictReader(f)
+
+        return [
+            {
+                "date": row["Date"],
+                "description": row["Description"],
+                "category": row["Category"],
+                "amount": row["Amount"],
+                "account": row["Account"],
+                "account_number": row["Account #"],
+                "institution": row["Institution"],
+                "month": row["Month"],
+                "week": row["Week"],
+                "transaction_id": row["Transaction ID"],
+                "account_id": row["Account ID"],
+                "check_number": row["Check Number"],
+                "full_description": row["Full Description"],
+                "date_added": row["Date Added"],
+            }
+            for row in r
+        ]
+
+
+def transaction_to_row(t: Transaction) -> dict:
+    return {
+        "Date": t["date"],
+        "Description": t["description"],
+        "Category": t["category"],
+        "Amount": t["amount"],
+        "Account": t["account"],
+        "Account #": t["account_number"],
+        "Institution": t["institution"],
+        "Month": t["month"],
+        "Week": t["week"],
+        "Transaction ID": t["transaction_id"],
+        "Account ID": t["account_id"],
+        "Check Number": t["check_number"],
+        "Full Description": t["full_description"],
+        "Date Added": t["date_added"],
+    }
 
 
 def get_transaction_fields():
-    with open(transaction_file, "r") as trans_f:
-        first = trans_f.readline()
+    with open(transaction_file, "r") as f:
+        first = f.readline()
         return first.strip().split(",")
 
 
-cats = read_categories()
-trans = read_transactions()
-accts = read_accounts()
+all_transactions = read_transactions()
+accounts = read_accounts()
 
 
-def get_example_trans():
-    categorized = [t for t in trans if t.get("Category")]
+def get_example_transactions() -> List[Transaction]:
+    categorized = [t for t in all_transactions if t.get("category")]
 
     examples = []
     seen_descriptions = set()
     for t in categorized:
-        if t["Description"] in seen_descriptions:
+        if t["description"] in seen_descriptions:
             continue
 
-        seen_descriptions.add(t["Description"])
+        seen_descriptions.add(t["description"])
 
         examples.append(t)
 
     return examples
 
 
-def get_example_trans_docs():
-    return [
-        f"Transaction with description '{t['Description']}', Account '{t['Account']}' from institution '{t['Institution']}' "
-        + f"was categorized as '{t['Category']}'"
-        for t in example_trans
-    ]
+def format_transaction(t: Transaction) -> str:
+    return "\n".join((f"{k}: '{v}'" for k, v in t.items() if k and v))
 
 
-example_trans = get_example_trans()
+def get_multiple_tries(tx: Transaction) -> Optional[Categorization]:
+    max_cat: Categorization = None
+    for i in range(max_attempts):
+        try:
+            graph = (
+                StateGraph(State)
+                .add_sequence([retrieve_previous_txs, categorize])
+                .add_edge(START, "retrieve_previous_txs")
+                .compile()
+            )
+
+            response: State = graph.invoke({"transaction": tx})
+
+            cat = massage_categorization(response["categorization"])
+
+            if cat.confidence_level >= high_confidence_level:
+                log.info(f"Categorized with high confidence after {i+1} attempts")
+                return cat
+            elif cat.confidence_level >= confidence_cuttoff:
+                if not max_cat:
+                    max_cat = cat
+                elif cat.confidence_level > max_cat.confidence_level:
+                    max_cat = cat
+        except Exception as e:
+            log.info(f"Graph failed: {e}")
+            continue
+
+    if not max_cat:
+        log.error(f"Failed to get data after {max_attempts} attempts.")
+
+    return max_cat
 
 
-def build_prompt(t, cat_response) -> str:
-    accts_text = "\n".join(
-        (
-            f"Account Name: {a["Account"]}, Account Type: {a["Group"]}, Account Class: {a["Class"]}"
-            for a in accts
-        )
+embeddings = OllamaEmbeddings(model=embedding_model, keep_alive=10 * 60)
+
+vector_store = InMemoryVectorStore(embeddings)
+
+example_tx_docs = [
+    Document(
+        id=t["transaction_id"],
+        page_content=format_transaction(t),
+        metadata={"source": "transaction_file"},
     )
-    t_text = "\n".join((f"{k}: {v}" for k, v in t.items()))
+    for t in get_example_transactions()
+]
 
-    example_t_text = ""
-    if cat_response:
-        example_t_text = f"""
-Here is a possibly related previous categorization, if this categorization matches the
+docs = vector_store.add_documents(
+    documents=example_tx_docs,
+)
+
+
+def retrieve_previous_txs(state: State):
+    # TODO I don't think embedding models are capable of returning nothing
+    # They always provide some context
+    prev_tx_tmpl = ChatPromptTemplate.from_template(
+        """Which category was the transaction with the description '{description}',
+account '{account}', and institution '{institution}' categorized as?
+Say there were no previous categorizations if there were none."""
+    )
+
+    similarity_prompt = prev_tx_tmpl.invoke(
+        {
+            "description": state["transaction"]["description"],
+            "account": state["transaction"]["account"],
+            "institution": state["transaction"]["institution"],
+        }
+    )
+
+    prompt = similarity_prompt.to_messages()[0].content
+
+    log.debug(f"Retrieve previous categorizations prompt {prompt}")
+
+    context_res = vector_store.similarity_search(prompt)
+
+    log.debug(f"Retreived {len(context_res)} similar transactions for context")
+
+    return {"previous_transactions": context_res}
+
+
+def massage_categorization(c: Categorization):
+    if c.confidence_level == 1:
+        c.confidence_level = 100
+
+    if c.confidence_level < 1 and c.confidence_level > 0:
+        c.confidence_level = int(c.confidence_level * 100)
+
+    return c
+
+
+def categorize(state: State):
+    template = ChatPromptTemplate.from_template(
+        """Categorize the following transaction.
+
+Here's the list of accounts that the transaction may have come from for context:
+
+```
+{accounts}
+```
+
+Here is a possibly related previous categorizations, if this categorization matches the
 transaction that is provided, you should prefer this previous categorization over 
 inferring them from the transaction's fields:
 
 ```
-{cat_response}
+{previous_transactions}
+```
+
+And here is the transaction that needs to be categorized:
+
+```
+{transaction_text}
 ```
 """
-
-    c_text = "\n".join(cats)
-
-    return f"""Please categorize the following financial transaction.
-{example_t_text}
-
-For context, this transaction may be from one of the following accounts:
-
-```
-{accts_text}
-```
-
-The transaction will fall into one of the following categories:
-
-```
-{c_text}
-```
-
-Finally, here is the transaction information:
-
-```
-{t_text}
-```
-
-I will ask you to output a confidence level as a percentage. Don't be over-confident. 
-If there is ambiguity in the data provided, you should provide a lower confidence level.
-
-Output the response in the following json format, only output the json and nothing else:
-
-```json
-{{
-    "transactionId": "(string) The transaction id from the supplied transaction",
-    "accountId": "(string) The account id from the supplied transaction",
-    "category": "(string) The category from the supplied list of categories",
-    "confidenceLevel": "(number) The confidence level (in percent) in your determination of the category",
-    "explanation": "(string) A brief explanation as to how you made the categorization determination"
-}}
-```
-"""
-
-
-def validate_response_data(dat, transaction):
-    if dat["category"] not in cats:
-        raise Exception(f"Got unexpected category {dat['category']}")
-
-    lvl = dat["confidenceLevel"]
-    if lvl <= 0 or lvl > 100:
-        raise Exception(f"Got unexpected confidence level {lvl} {lvl}")
-
-    if dat["transactionId"] != transaction["Transaction ID"]:
-        raise Exception(f"Got unexpected transaction id")
-
-    if dat["accountId"] != transaction["Account ID"]:
-        raise Exception(f"Got unexpected account id")
-
-
-def massage_data(dat):
-
-    lvl = dat["confidenceLevel"]
-    new_lvl = None
-    if isinstance(lvl, int) and lvl in (1, 100):
-        new_lvl = lvl
-    elif isinstance(lvl, float) and lvl > 0 and lvl <= 1:
-        new_lvl = int(lvl * 100)
-    else:
-        # Prob a string
-        try:
-            new_lvl = int(lvl)
-        except:
-            new_lvl = int(float(lvl))
-
-    log.debug(
-        f"Converted confidence from {type(lvl)} {lvl} -> {type(new_lvl)} {new_lvl}"
     )
 
-    return {
-        **dat,
-        "confidenceLevel": new_lvl,
-    }
-
-
-rex = re.compile(f"```(json)?(.+)```", re.DOTALL | re.IGNORECASE | re.MULTILINE)
-
-
-def extract_data(response, transaction):
-    try:
-        try:
-            res = json.loads(response)
-        except:
-            match = rex.search(response)
-            if not match:
-                log.info(f"Failed to find json in response: {response}")
-                return None
-
-            res = json.loads(match.groups()[1])
-
-        log.debug(f"Parsed json {res}")
-
-        res = massage_data(res)
-
-        validate_response_data(res, transaction)
-
-        return res
-    except Exception as e:
-        log.info(f"Got an exception while extracting data {e}")
-        return None
-
-
-confidence_cuttoff = 95
-
-high_confidence_level = 97
-
-max_attempts = 5
-
-
-def choose_category(datas):
-    cat_sums = {}
-    for d in datas:
-        cat = d["category"]
-        confidence = d["confidenceLevel"]
-        if confidence < confidence_cuttoff:
-            continue
-
-        if cat in cat_sums:
-            cat_sums[cat] = max(confidence, cat_sums[cat])
-        else:
-            cat_sums[cat] = confidence
-
-    if not cat_sums:
-        return None
-
-    sums_sorted = sorted(cat_sums.items(), key=lambda x: x[1], reverse=True)
-
-    max_confidence = sums_sorted[0][1]
-
-    if max_confidence < confidence_cuttoff:
-        log.info(f"Skipping {datas[0]["transactionId"]} because it had confidence below cuttoff of: {max_confidence}")
-        return None
-
-    return sums_sorted[0][0]
-
-
-def get_multiple_tries(prompt, transaction):
-
-    datas = []
-    for i in range(max_attempts):
-        response: ChatResponse = chat(
-            model=model,
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                },
-            ],
-            keep_alive="10m",
+    accts_text = "\n\n".join(
+        (
+            f"Account Name: '{a["account"]}', Account Type: '{a["group"]}', Account Class: '{a["account_class"]}'"
+            for a in accounts
         )
-
-        response_content = response["message"]["content"]
-
-        log.debug(response_content)
-
-        data = extract_data(response_content, transaction)
-        if data:
-            datas.append(data)
-
-            if data["confidenceLevel"] >= high_confidence_level:
-                return (i + 1, datas)
-
-    if not datas:
-        log.error(f"Failed to get data after {max_attempts} attempts.")
-
-    return (max_attempts, datas)
-
-
-def generate_categorization_query_prompt(t):
-    return (
-        f"Which category was the transaction with the description '{t['Description']}', "
-        + f" account '{t['Account']}' and institution '{t["Institution"]}' "
-        + f"categorized as? Say there were no previous categorizations if there were none."
     )
 
+    transaction_text = format_transaction(state["transaction"])
+    previous_tx_text = "\n\n".join(
+        (", ".join(c.page_content.split("\n")) for c in state["previous_transactions"])
+    )
 
-client = chromadb.Client()
-collection = client.create_collection(name="example_transactions")
+    structured_llm = llm.with_structured_output(Categorization)
 
-# store each document in a vector embedding database
-for i, d in enumerate(get_example_trans_docs()):
-    response = ollama.embed(model=embedding_model, input=d)
-    embeddings = response["embeddings"]
-    collection.add(ids=[str(i)], embeddings=embeddings, documents=[d])
+    prompt = template.invoke(
+        {
+            "accounts": accts_text,
+            "previous_transactions": previous_tx_text,
+            "transaction_text": transaction_text,
+        }
+    )
+
+    log.debug(f"The full prompt: {prompt.to_messages()[0].content}")
+
+    response = structured_llm.invoke(prompt)
+
+    return {"categorization": response}
 
 
 with open(output_file, "w", newline="", encoding="utf-8") as out_f:
@@ -320,51 +341,30 @@ with open(output_file, "w", newline="", encoding="utf-8") as out_f:
     w.writeheader()
 
     start = time.time()
-    for t in trans:
+    for tx in all_transactions:
         # Already categorized
-        if t.get("Category"):
-            w.writerow(t)
+        if tx["category"].strip():
+            w.writerow(transaction_to_row(tx))
             out_f.flush()
             log.info("Line is already categorized, skipping.")
             continue
 
         log.info("Categorizing line")
 
-        # generate an embedding for the input and retrieve the most relevant doc
-        cat_query_prompt = generate_categorization_query_prompt(t)
+        cat = get_multiple_tries(tx)
+        category = tx["category"]
 
-        log.info(f"Prompt for categorization query: {cat_query_prompt}")
+        if cat:
+            log.info(
+                f"Categorized {tx["description"]} -- {cat.category}, {cat.confidence_level} -- {cat.explanation}"
+            )
+            category = cat.category
+        else:
+            log.info(f"Failed to categorize transaction {tx}")
 
-        response = ollama.embed(model=embedding_model, input=cat_query_prompt)
-
-        results = collection.query(query_embeddings=response["embeddings"], n_results=1)
-
-        cat_response = results["documents"][0][0]
-
-        log.info(f"Categorization query response: {cat_response}")
-
-        prompt = build_prompt(t, cat_response)
-
-        log.debug(f"Full prompt: {prompt}")
-
-        num_tokens = num_tokens_from_string(prompt, "o200k_base")
-
-        log.debug(f"This prompt contains {num_tokens} tokens.")
-
-        attempts, datas = get_multiple_tries(prompt, t)
-
-        log.info(f"Got response in {attempts} tries.")
-
-        cat = choose_category(datas)
-
-        log.info(f"Categorized {cat} from {t}")
-
-        out_dict = {
-            **t,
-            "Category": cat,
-        }
+        out_dict = transaction_to_row({**tx, "category": category})
 
         w.writerow(out_dict)
         out_f.flush()
 
-    print(f"Processed {len(trans)} in {time.time() - start} seconds")
+    log.info(f"Processed {len(all_transactions)} in {time.time() - start} seconds")
